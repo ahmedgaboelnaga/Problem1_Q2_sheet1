3ae16496456e5cb61de82c13d37802879d2fc294
ahmedgaboelnaga <ahmedgaboelnaga@gmail.com>
2023-03-12 21:49:31 +0200

    comment

--- Problem1_Q2/FileName.cpp
+++ Problem1_Q2/FileName.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main()
 {
+	//2.Given a positive number, check the negative or positive, print the number and its type in a message.
+
 	double n;
 
 	cout << "Enter the number: ";
